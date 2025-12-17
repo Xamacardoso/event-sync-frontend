573f8783b7c8c687fd4293c1ddfdb968a62d9444
Xamacardoso <contato.xcardoso@gmail.com>
2025-12-17 09:34:00 -0300

    :gear: Chore (ENV): Added env variable for API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // URL do seu Backend NestJS
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
 });
 
 // Interceptador para adicionar o Token JWT em toda requisição
